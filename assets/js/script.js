ab0cb2f562c8aff5159e1167818966e0653f7138
NeXFP <giovannycardoso50@gmail.com>
2021-09-23 06:45:40 -0400

    Added variables to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    var time = document.querySelectorAll("");
+    var userInput = document.querySelectorAll("")
+    var time = document.querySelectorAll("");
+    var save = document.querySelectorAll("");
+    var hour = moment().format("HH");
+    $("currentDate").text(moment(format.("dd, MM, YY"));
+})
